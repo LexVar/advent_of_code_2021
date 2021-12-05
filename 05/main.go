b14b5c1d80e44a6e04ad406bb7f1898760706f99
Alexandre Rodrigues <avalente@protonmail.com>
2021-12-05 10:51:01 +0000

    Optimization - eliminates final loop

--- 05/main.go
+++ 05/main.go
@@ -19,7 +19,8 @@ func read_lines () ([][]int) {
 	return lines
 }
 
-func draw_board (lines [][]int) ([][]int) {
+func draw_board (lines [][]int) ([][]int, int) {
+	count := 0
 	// Initialize 9x9 board
 	board := make([][]int, BOARD_SIZE)
 	for i := 0; i < BOARD_SIZE; i++ {
@@ -28,42 +29,45 @@ func draw_board (lines [][]int) ([][]int) {
 			board[i][j] = 0
 		}
 	}
+
 	for _, line := range lines {
 		// If Xs are equal, or Ys are equal
 		if line[0] == line[2] {
 			r := python_range(line[1],line[3])
 			for _, y := range r {
 				board[line[0]][y]++
+				// Count on the spot
+				if board[line[0]][y] == 2 {
+					count++
+				}
 			}
 		} else if line[1] == line[3] {
 			r := python_range(line[2],line[0])
 			for _, x := range r {
 				board[x][line[1]]++
+				// Count on the spot
+				if board[x][line[1]] == 2 {
+					count++
+				}
 			}
 		} else {
 			xr := python_range(line[0],line[2])
 			yr := python_range(line[1],line[3])
 			for i := 0; i < len(xr); i++ {
 				board[xr[i]][yr[i]]++
+				// Count on the spot
+				if board[xr[i]][yr[i]] == 2 {
+					count++
+				}
 			}
 		}
 	}
 
-	return board
+	return board, count
 }
 
-func count_points (board [][]int) (int) {
-	count := 0
-	for _,line := range board {
-		for _,point := range line {
-			if point > 1 {
-				count++
-			}
-		}
-	}
-	return count
-}
-
+// Emulates python's range function
+// Create list of consecutive numbers between two integers
 func python_range(n1 int, n2 int) ([]int) {
 	hi, lo := 0, 0
 	if (n1 < n2) {
@@ -90,13 +94,8 @@ func python_range(n1 int, n2 int) ([]int) {
 
 func main () {
 	lines := read_lines ()
-	fmt.Println(lines)
 
-	board := draw_board(lines)
-	for _, line := range board {
+	_, count := draw_board(lines)
 
-		fmt.Println(line)
-	}
-	count := count_points(board)
 	fmt.Println(count)
 }
